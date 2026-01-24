cecb7d4039058a1141fc280946dea71a1bff0b5d
MCoder1013 <mattdevaney1@gmail.com>
2026-01-24 13:42:45 -0600

    changed message on index.ts - testing git feature

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.get('/', (_req, res) => {
-  res.send('Hello, TypeScript with Node.js!');
+  res.send('Welcome to the reservation system!');
 });
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
