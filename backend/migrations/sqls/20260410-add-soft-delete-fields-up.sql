70df464c6f13dd95f6519cc353a1c1cc5e549238
m <example@example.com>
2026-04-14 05:02:58 +0600

    delete incorrect sqls

--- backend/migrations/sqls/20260410-add-soft-delete-fields-up.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE users ADD COLUMN deleted_at TIMESTAMPTZ; -- Add soft delete fields to all the tables
-ALTER TABLE resources ADD COLUMN deleted_at TIMESTAMPTZ;
-ALTER TABLE cabins ADD COLUMN deleted_at TIMESTAMPTZ;
-ALTER TABLE staff ADD COLUMN deleted_at TIMESTAMPTZ;
